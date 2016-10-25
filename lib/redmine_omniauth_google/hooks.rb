99877e71be547cbb8ade96c0fc93b5d66bc7fdff
Andriy Lesyuk <s-andy@andriylesyuk.com>
2016-10-25 20:02:38 +0300

    Change the sign-in button (for TweetBook)

--- lib/redmine_omniauth_google/hooks.rb
+++ lib/redmine_omniauth_google/hooks.rb
@@ -1,9 +1,5 @@
 module RedmineOmniauthGoogle
   class Hooks < Redmine::Hook::ViewListener
-    def view_account_login_bottom(context = {})
-      context[:controller].send(:render_to_string, {
-        :partial => "hooks/view_account_login_bottom",
-        :locals => context})
-    end
+    render_on :view_account_login_top, :partial => 'hooks/view_account_login_top'
   end
 end
